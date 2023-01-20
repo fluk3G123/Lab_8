f3694ac1ff55129caa3ec1f13bc98ff5bb05b255
___.flxk.__ <89276072+fluk3G123@users.noreply.github.com>
2023-01-20 17:22:42 +0700

    good

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,18 +1,28 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+#include<sstream>
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+using namespace std;
+
+int main(){
+    string name,movie,day,qoute;
+    int id,gear = id-12000000;
+    string x=to_string(gear);
+    
+
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?";
+    cin>>name;
+    cout<<endl<<"Fahsai: " <<name<<"is a really cool name.\n";
+    cout<<"Fahsai: I think you are an Engineering student. What is your student ID?";
+    cin>>id;
+    cout<<"\nFahsai: I think you may be GEAR"<<gear<< "I have a free movie ticket for you.";
+    cout<<"Fahsai: Let's go to the cinema together!!!\nFahsai: What movie do you want to watch?\n";
+    cin>>movie;
+    cout<<"\nFahsai: So....which day are you free to go with me?";
+    cin>>day;
+    cout<<endl<<day<<"....that is OK!!! I'm looking forward to watching"<<movie<<"with you.\n";
+    cin>>qoute;
+    cout<<"555+ see you"<<day<<". Bye Bye \(^ ^)/";
+
+
+}
